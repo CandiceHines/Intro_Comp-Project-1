9bc1ed1616c60d57a993073e7d57f2d0ab2be69e
Candice <candicehines17@gmail.com>
2022-09-28 21:58:13 -0700

    animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+# Author: Candice Hines
+# GitHub username: CandiceHines
+# Date: 09/28/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
+
